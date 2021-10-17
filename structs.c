ba3bbf566347ae37dd4aedf62968f854e286121b
unknown <>
2021-10-17 19:07:58 -0400

    Fixes error with string literals

--- structs.c
+++ structs.c
@@ -15,14 +15,15 @@ struct operator * heapStruct(char *a, char *b, int c) {
 
   struct operator *n = malloc(sizeof(struct operator));
 
-  n->name =  malloc(strlen(a));
+  n->name =  malloc(strlen(a) + 1);
   strcpy(n->name,a);
-  n->class = malloc(strlen(b));
+  n->class = malloc(strlen(b) + 1);
   strcpy(n->class,b);
   n->cost = c;
   n->next = NULL;
   return n;
 }
+
 void returnStruct(struct operator *o) {
   printf("%s is a %s that costs %d deployment points\n", o->name, o->class, o->cost);
 }
