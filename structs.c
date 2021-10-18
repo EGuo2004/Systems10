bdfe69e22e54bf2f261d1134b7a36a15c1c714df
Eric Guo <72960536+EGuo2004@users.noreply.github.com>
2021-10-18 11:45:26 -0400

    Update structs.c

--- structs.c
+++ structs.c
@@ -49,13 +49,3 @@ struct operator * free_list(struct operator *o) {
   free(o->class);
   free(o);
 }
-
-int main() {
-  struct operator *W = heapStruct("W", "Splash Sniper", 26);
-  struct operator *Ines = insert_front(W,"Ines", "Reaper", 21);
-  struct operator *Rosmontis = insert_front(Ines,"Rosmontis", "Bombardier", 23);
-
-  print_list(Rosmontis);
-  free_list(Rosmontis);
-  return 0;
-}
